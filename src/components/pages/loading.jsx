6c07e0c549077be7a1e2fa618b13e1bed27e0d29
Muhammad Huzaifa <hajihuzaifa9612@gmail.com>
2023-03-05 23:40:22 +0500

    some changes in load.jsx

--- src/components/pages/loading.jsx
+++ src/components/pages/loading.jsx
@@ -1,19 +1,25 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import "../styles/loading.css"
 
 // import loadImg from "../PNGs/load.jpg"
 
-export default function Loading(){
-useEffect(()=>{
-document.title = "Loading ..."
-},[])
+export default function Loading() {
+    const [isVisible, setIsVisible] = useState(false);
+    useEffect(() => {
+        document.title = "APIs Hub | Loading ...";
+        setTimeout(()=>setIsVisible(true), 5000)
 
-return(<>
-<div className="loadMsgContainer">
-    <p className="blue">server might be Sleeping 😴😴 | Gurbat cheekh cheekh k bol rhi hey 😭😭</p>
-<p className="blue">This might Take  __ mins 🙄🙄 | Please have some coffee ☕☕</p>
-</div>
-    <div className ="load"></div>
-</>
-)
+    }, [])
+
+    return (<>
+        {isVisible && <>
+            <div className="loadMsgContainer">
+                <p className="blue">Server might be Sleeping 😴😴 | Gurbat cheekh cheekh k bol rhi hey 😭😭</p>
+                <p className="blue">This might Take  __ mins 🙄🙄 | Please have some coffee ☕☕</p>
+            </div>
+            </>
+            }
+        <div className="load"></div>
+    </>
+    )
 }
